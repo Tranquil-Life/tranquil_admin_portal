cacd7bf580bde32f4f3e421c2a08516c794b2307
SKPnation <ayomideseaz@gmail.com>
2025-03-08 17:02:15 -0600

    replaced the revenue pie chart

--- lib/features/dashboard/presentation/widgets/app_installs/installs_chart.dart
+++ lib/features/dashboard/presentation/widgets/app_installs/installs_chart.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart' as charts;
 import 'package:tranquil_admin_portal/features/dashboard/data/models/app_installs_model.dart';
 
 class InstallsChart extends StatelessWidget {
@@ -9,47 +8,48 @@ class InstallsChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return charts.SfCartesianChart(
-        primaryXAxis: charts.CategoryAxis(),
-        legend: const charts.Legend(isVisible: true),
-        // Enable tooltip
-        tooltipBehavior: charts.TooltipBehavior(enable: true),
-        series: <charts.ChartSeries<AppInstalls, String>>[
-          charts.StackedBarSeries<AppInstalls, String>(
-              dataSource: data!,
-              xValueMapper: (AppInstalls installs, _) => installs.os,
-              yValueMapper: (AppInstalls installs, _) => installs.installs,
-              name: 'Installs',
-              onCreateRenderer: (charts.ChartSeries<AppInstalls, String> series) =>
-                  _CustomColumnSeriesRenderer(),
-        isVisibleInLegend: false,
-              // Enable data label
-              dataLabelSettings: charts.DataLabelSettings(isVisible: true))
-        ]);
+    return SizedBox();
+      // charts.SfCartesianChart(
+      //   primaryXAxis: charts.CategoryAxis(),
+      //   legend: const charts.Legend(isVisible: true),
+      //   // Enable tooltip
+      //   tooltipBehavior: charts.TooltipBehavior(enable: true),
+      //   series: <charts.ChartSeries<AppInstalls, String>>[
+      //     charts.StackedBarSeries<AppInstalls, String>(
+      //         dataSource: data!,
+      //         xValueMapper: (AppInstalls installs, _) => installs.os,
+      //         yValueMapper: (AppInstalls installs, _) => installs.installs,
+      //         name: 'Installs',
+      //         onCreateRenderer: (charts.ChartSeries<AppInstalls, String> series) =>
+      //             _CustomColumnSeriesRenderer(),
+      //   isVisibleInLegend: false,
+      //         // Enable data label
+      //         dataLabelSettings: charts.DataLabelSettings(isVisible: true))
+      //   ]);
   }
 }
 
-class _CustomColumnSeriesRenderer extends charts.StackedBarSeriesRenderer {
-  _CustomColumnSeriesRenderer();
-
-  @override
-  charts.StackedBarSegment createSegment() {
-    return _ColumnCustomPainter();
-  }
-}
-
-class _ColumnCustomPainter extends charts.StackedBarSegment {
-  final colorList = [
-    Colors.brown,
-    Color(0xff00649F),
-  ];
-
-  @override
-  Paint getFillPaint() {
-    final Paint customerFillPaint = Paint();
-    customerFillPaint.isAntiAlias = false;
-    customerFillPaint.color = colorList[currentSegmentIndex!];
-    customerFillPaint.style = PaintingStyle.fill;
-    return customerFillPaint;
-  }
-}
+// class _CustomColumnSeriesRenderer extends charts.StackedBarSeriesRenderer {
+//   _CustomColumnSeriesRenderer();
+//
+//   @override
+//   charts.StackedBarSegment createSegment() {
+//     return _ColumnCustomPainter();
+//   }
+// }
+//
+// class _ColumnCustomPainter extends charts.StackedBarSegment {
+//   final colorList = [
+//     Colors.brown,
+//     Color(0xff00649F),
+//   ];
+//
+//   @override
+//   Paint getFillPaint() {
+//     final Paint customerFillPaint = Paint();
+//     customerFillPaint.isAntiAlias = false;
+//     customerFillPaint.color = colorList[currentSegmentIndex!];
+//     customerFillPaint.style = PaintingStyle.fill;
+//     return customerFillPaint;
+//   }
+// }
