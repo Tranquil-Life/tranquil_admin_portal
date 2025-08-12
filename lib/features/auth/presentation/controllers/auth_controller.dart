abd2679e05c32ca005fe0e0461d6ce343cd97912
SKPnation <ayomideseaz@gmail.com>
2025-08-12 14:15:28 -0500

    fixed log out bug

--- lib/features/auth/presentation/controllers/auth_controller.dart
+++ lib/features/auth/presentation/controllers/auth_controller.dart
@@ -6,6 +6,7 @@ import 'package:tranquil_admin_portal/core/constants/endpoints.dart';
 import 'package:tranquil_admin_portal/core/constants/theme/app_colors.dart';
 import 'package:tranquil_admin_portal/core/data/local/get_store.dart';
 import 'package:tranquil_admin_portal/core/global/custom_snackbar.dart';
+import 'package:tranquil_admin_portal/core/utils/helpers/navigation/app_routes.dart';
 import 'package:tranquil_admin_portal/core/utils/services/api_service.dart';
 import 'package:tranquil_admin_portal/features/auth/data/repos/auth_repo.dart';
 import 'package:tranquil_admin_portal/features/auth/domain/entities/login_data.dart';
@@ -63,6 +64,8 @@ class AuthController extends GetxController {
     if(response.statusCode == 200){
       signOut = true;
     }else{
+      Get.offAllNamed(Routes.authenticationPageRoute);
+      getStore.clearAllData();
       signOut = false;
     }
 
